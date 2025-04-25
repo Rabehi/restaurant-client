b7d59d2279e99a1d6c1989d80a937a2bc0216fd5
Rafa Bernardino <rafa.bh11@gmail.com>
2025-04-25 11:33:02 +0200

    ordernar mesas por la query del backend

--- src/context/MesasStore.ts
+++ src/context/MesasStore.ts
@@ -41,7 +41,7 @@ export const useMesasStore = create<MesasStore>((set, get) => {
         try {
             const { data } = await axios.get("http://localhost:3000/mesas");
             set({
-                mesas: data.sort((a: Mesa, b: Mesa) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
+                mesas: data
             });
         } catch (error) {
             console.error("Error al cargar mesas:", error);
